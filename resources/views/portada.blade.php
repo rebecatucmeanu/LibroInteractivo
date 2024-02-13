6405b432825bf992d50e6ba6defaceeebd29cc0e
rebecatucmeanu <atp0011@alu.medac.es>
2024-02-13 13:10:10 +0100

    final

--- resources/views/portada.blade.php
+++ resources/views/portada.blade.php
@@ -1,7 +1,6 @@
 <body>
     <h1>{{ $title }}</h1>
     <p>Author: {{ $author }}</p>
-    <p>Nombre: {{Session::get('nombreUsuario')}}</p>
     <img src="{{asset('img/portada.jpg')}}" alt="portada">
     <a href="{{url('capitulo1')}}"><button>Start</button></a>
 </body>
